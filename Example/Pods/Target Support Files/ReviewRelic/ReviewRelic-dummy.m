777887db229ddf6f5773d9abc98453688e7e770f
raheelsadiq <raheel.sadiq.08@gmail.com>
2021-02-23 23:13:10 +0500

    Podspec setup

---
+++ Example/Pods/Target Support Files/ReviewRelic/ReviewRelic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ReviewRelic : NSObject
+@end
+@implementation PodsDummy_ReviewRelic
+@end
---
+++ Example/ReviewRelic/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ReviewRelic
+//
+//  Created by raheel-sadiq-zameen on 02/23/2021.
+//  Copyright (c) 2021 raheel-sadiq-zameen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
